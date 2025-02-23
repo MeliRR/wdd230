f16b44e81d88155cfebb8d29568b5ae83f20f04f
MeliRR <rey15016@byui.edu>
2025-02-23 04:38:11 -0600

    lesson3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,9 @@ document.getElementById("current-year").textContent = new Date().getFullYear();
 
 // Get the last modified date of the document and set it in the footer
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+// Toggle the visibility of the menu when the hamburger button is clicked
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    var menu = document.getElementById('nav-menu');
+    menu.classList.toggle('hidden');  // Toggle the 'hidden' class
+});
